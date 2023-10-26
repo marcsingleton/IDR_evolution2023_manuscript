d69d125b0b350d602d7821841f318a2d36beaaa2
Marc Singleton <marcsingleton@berkeley.edu>
2023-10-26 16:38:09 -0700

    Changed order of panels and shifted right

--- figures/simulation/simulation.py
+++ figures/simulation/simulation.py
@@ -26,7 +26,7 @@ if not os.path.exists('out/'):
 # === MAIN FIGURE ===
 fig = plt.figure(figsize=(7.5, 4.5))
 gs = plt.GridSpec(2, 3)
-gridspec_kw = {'left': 0.2, 'right': 0.9, 'bottom': 0.2, 'top': 0.9}
+gridspec_kw = {'left': 0.25, 'right': 0.95, 'bottom': 0.2, 'top': 0.9}
 
 # BM MODEL PLOTS
 # --- PANEL A: Violin plots of sigma2_BM ---
@@ -42,7 +42,7 @@ ax.set_xlabel('true $\mathregular{\log_{10}(\sigma^2_{BM})}$')
 ax.set_ylabel('estimated $\mathregular{\log_{10}(\sigma^2_{BM})}$')
 subfig.suptitle('A', x=0.025, y=0.975, fontweight='bold')
 
-# --- PANEL B: Type I error as function of critical value ---
+# --- PANEL D: Type I error as function of critical value ---
 delta_loglikelihood_min, delta_loglikelihood_max = df_BM['delta_loglikelihood'].min(), df_BM['delta_loglikelihood'].max()
 critvals = np.linspace(delta_loglikelihood_min, delta_loglikelihood_max, 50)
 
@@ -51,7 +51,7 @@ for critval in critvals:
     errors.append(groups_BM['delta_loglikelihood'].aggregate(lambda x: (x > critval).mean()))
 errors = pd.DataFrame(errors).reset_index(drop=True)
 
-subfig = fig.add_subfigure(gs[0, 1], facecolor='none')
+subfig = fig.add_subfigure(gs[1, 0], facecolor='none')
 ax = subfig.subplots(gridspec_kw=gridspec_kw)
 id2value = groups_BM['sigma2'].mean().apply(np.log10).to_dict()
 cmap = ListedColormap(plt.colormaps['viridis'].colors[:240])
@@ -62,16 +62,16 @@ for sigma2_id in errors:
 ax.set_xlabel('$\mathregular{\log L_{OU} / L_{BM}}$ critical value')
 ax.set_ylabel('Type I error')
 subfig.colorbar(ScalarMappable(norm=norm, cmap=cmap))
-subfig.suptitle('B', x=0.025, y=0.975, fontweight='bold')
+subfig.suptitle('D', x=0.025, y=0.975, fontweight='bold')
 
-# --- PANEL C: Type I error as function of critical value (merge) ---
+# --- PANEL E: Type I error as function of critical value (merge) ---
 errors = []
 for critval in critvals:
     errors.append((df_BM['delta_loglikelihood'] > critval).mean())
 q95 = df_BM['delta_loglikelihood'].quantile(0.95)
 q99 = df_BM['delta_loglikelihood'].quantile(0.99)
 
-subfig = fig.add_subfigure(gs[0, 2], facecolor='none')
+subfig = fig.add_subfigure(gs[1, 1], facecolor='none')
 ax = subfig.subplots(gridspec_kw=gridspec_kw)
 ax.plot(critvals, errors)
 ax.axvline(q95, color='C1', label='5%')
@@ -81,7 +81,7 @@ ax.set_ylabel('Type I error')
 ax.legend(title='Type I error',
           loc='center', bbox_to_anchor=(0.9, 0.5),
           title_fontsize=8, fontsize=8)
-subfig.suptitle('C', x=0.025, y=0.975, fontweight='bold')
+subfig.suptitle('E', x=0.025, y=0.975, fontweight='bold')
 
 # OU MODEL PLOTS
 df_parameter = groups_OU[['sigma2_hat_OU', 'alpha_hat_OU', 'sigma2', 'alpha']].mean().sort_index(level=['sigma2_id', 'alpha_id'])
@@ -91,11 +91,11 @@ df_error = (groups_OU['delta_loglikelihood'].aggregate(**{'q95': lambda x: (x < 
 extent = (alpha_min-alpha_delta/2, alpha_max+alpha_delta/2,
           sigma2_min-sigma2_delta/2, sigma2_max+sigma2_delta/2)
 
-# --- PANEL D: Heatmap of sigma2 ---
+# --- PANEL B: Heatmap of sigma2 ---
 array = np.log10((df_parameter['sigma2_hat_OU'] / df_parameter['sigma2']).to_numpy()).reshape((sigma2_num, alpha_num))
 vext = max(abs(array.min()), abs(array.max()))
 
-subfig = fig.add_subfigure(gs[1, 0], facecolor='none')
+subfig = fig.add_subfigure(gs[0, 1], facecolor='none')
 ax = subfig.subplots(gridspec_kw=gridspec_kw)
 im = ax.imshow(array,
                extent=extent, origin='lower', aspect='auto',
@@ -103,13 +103,13 @@ im = ax.imshow(array,
 ax.set_xlabel('true $\mathregular{\log_{10}(\\alpha)}$')
 ax.set_ylabel('true $\mathregular{\log_{10}(\sigma^2_{OU})}$')
 fig.colorbar(im)
-subfig.suptitle('D', x=0.025, y=0.975, fontweight='bold')
+subfig.suptitle('B', x=0.025, y=0.975, fontweight='bold')
 
-# --- PANEL E: Heatmap of alpha ---
+# --- PANEL C: Heatmap of alpha ---
 array = np.log10((df_parameter['alpha_hat_OU'] / df_parameter['alpha']).to_numpy()).reshape((sigma2_num, alpha_num))
 vext = max(abs(array.min()), abs(array.max()))
 
-subfig = fig.add_subfigure(gs[1, 1], facecolor='none')
+subfig = fig.add_subfigure(gs[0, 2], facecolor='none')
 ax = subfig.subplots(gridspec_kw=gridspec_kw)
 im = ax.imshow(array,
                extent=extent, origin='lower', aspect='auto',
@@ -117,7 +117,7 @@ im = ax.imshow(array,
 ax.set_xlabel('true $\mathregular{\log_{10}(\\alpha)}$')
 ax.set_ylabel('true $\mathregular{\log_{10}(\sigma^2_{OU})}$')
 fig.colorbar(im)
-subfig.suptitle('E', x=0.025, y=0.975, fontweight='bold')
+subfig.suptitle('C', x=0.025, y=0.975, fontweight='bold')
 
 # --- PANEL F: Heatmap of type II errors ---
 subfig = fig.add_subfigure(gs[1, 2], facecolor='none')
